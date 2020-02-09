a8267608d45e12a07931adcc9b5ae752a6f98436
Ben Bryant <brbryant2639@gmail.com>
2020-02-09 10:42:17 -0600

    set html, completed steps 1-3 in app.js

---
+++ DOM/app.js
@@ -0,0 +1,53 @@
+let main = document.createElement('div');
+main.className = 'container';
+document.body.appendChild(main);
+
+// Step 1
+    let btnDiv = document.createElement('div');
+    btnDiv.className = 'row mt-4 justify-content-center'
+    main.appendChild(btnDiv);
+    let btn = document.createElement('button');
+    btn.textContent = 'Welcome';
+    btn.className = 'btn btn-primary';
+    btnDiv.appendChild(btn);
+    // Event Listener for clicks to button
+    btn.addEventListener('click', function(){
+        alert('Hello, Traveler!')
+    })
+
+// Step 2
+
+    // Div for button and text area
+    let firstDiv = document.createElement('div');
+    firstDiv.className = 'input-group my-3';
+    main.appendChild(firstDiv);
+    // Button
+    let inputBtn = document.createElement('button');
+    inputBtn.className = 'btn btn-primary mr-2';
+    inputBtn.textContent = 'Submit';
+    firstDiv.appendChild(inputBtn);
+    // Text Area
+    let textArea = document.createElement('textarea');
+    textArea.className = 'form-control';
+    firstDiv.appendChild(textArea);
+    // Event Listener for clicks on button to display text in textbox
+    inputBtn.addEventListener('click', function(){
+        alert(textArea.value);
+    })
+
+// Step 3
+    // Create new div
+    let twoDiv = document.createElement('div');
+    twoDiv.className = 'row border border-dark rounded';
+    twoDiv.style = 'height: 200px'
+    main.appendChild(twoDiv);
+    // Event Listener for mouseenter and mouseleave to change background color
+    twoDiv.addEventListener('mouseenter', e => {
+        twoDiv.style.backgroundColor = 'blue';
+    })
+    twoDiv.addEventListener('mouseleave', e => {
+        twoDiv.style.backgroundColor = 'white';
+    })
+
+// Step 4
+    
